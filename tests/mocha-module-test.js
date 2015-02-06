6967719fc006c43788fca62c37d417dea5bc06e5
Charles Lowell <cowboyd@frontside.io>
2015-02-06 12:35:24 -0600

    ensure that context is preserved in setup/teardown

---
+++ tests/mocha-module-test.js
@@ -0,0 +1,27 @@
+import { createModule } from 'ember-mocha/mocha-module';
+import { TestModule } from 'ember-test-helpers';
+
+describe('MochaModule', function() {
+  createModule(TestModule, 'component:x-foo', 'context', function() {
+    var thisInBefore, thisInBeforeEach;
+
+    before(function() {
+      thisInBefore = this;
+    });
+    beforeEach(function() {
+      thisInBeforeEach = this;
+    });
+    it("is preserved inside all assertions and hooks", function() {
+      expect(this).to.be.defined;
+      expect(this).to.equal(thisInBefore);
+      expect(this).to.equal(thisInBeforeEach);
+    });
+    afterEach(function() {
+      expect(this).to.equal(thisInBeforeEach);
+    });
+    after(function() {
+      expect(this).to.equal(thisInBefore);
+      expect(this).to.equal(thisInBeforeEach);
+    });
+  });
+});
