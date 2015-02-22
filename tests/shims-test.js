d63ed508ad7ee89fa0e80550b07d0054711899c1
Dan Gebhardt <dan@cerebris.com>
2015-02-22 14:20:41 -0500

    Fully test mocha shim.

--- tests/shims-test.js
+++ tests/shims-test.js
@@ -1,10 +1,15 @@
-import { describe, it } from 'mocha';
+import { mocha, describe, it, before, after, beforeEach, afterEach } from 'mocha';
 import { expect, assert } from 'chai';
 
 describe('mocha-shim', function() {
   it('should work', function() {
+    window.chai.expect(mocha).to.equal(window.mocha);
     window.chai.expect(describe).to.equal(window.describe);
     window.chai.expect(it).to.equal(window.it);
+    window.chai.expect(before).to.equal(window.before);
+    window.chai.expect(after).to.equal(window.after);
+    window.chai.expect(beforeEach).to.equal(window.beforeEach);
+    window.chai.expect(afterEach).to.equal(window.afterEach);
   });
 });
 
