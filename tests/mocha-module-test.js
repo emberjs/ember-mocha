fc534ff090bcb9c7981f22bcc04d5cc234b3da1d
Alex Gibbons <alex.gibbons@gmail.com>
2015-09-21 23:16:36 -0700

    Preserve suite context in outer test function

--- tests/mocha-module-test.js
+++ tests/mocha-module-test.js
@@ -1,10 +1,11 @@
 import { createModule } from 'ember-mocha/mocha-module';
 import { TestModule } from 'ember-test-helpers';
-import { describe, it, before, after, beforeEach, afterEach } from 'mocha';
+import { describe, it, before, after, beforeEach, afterEach, mocha } from 'mocha';
 import { expect } from 'chai';
 
 describe('MochaModule', function() {
   createModule(TestModule, 'component:x-foo', 'context', function() {
+    var testFunctionContext = this;
     var thisInBefore, thisInBeforeEach;
 
     before(function() {
@@ -17,6 +18,9 @@ describe('MochaModule', function() {
       expect(this).to.be.defined;
       expect(this).to.equal(thisInBefore);
       expect(this).to.equal(thisInBeforeEach);
+    });
+    it("preserves mocha suite context in outer test function", function() {
+      expect(testFunctionContext).to.be.an.instanceof(mocha.suite.constructor);
     });
     afterEach(function() {
       expect(this).to.equal(thisInBeforeEach);
