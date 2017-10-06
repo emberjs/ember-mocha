3f459c832977c79b23bbf1d4074daffeb51c4e2f
Dmitri Akatov <akatov@gmail.com>
2017-10-06 02:18:46 +0100

    additional tests for *.run hooks

--- tests/shims-test.js
+++ tests/shims-test.js
@@ -1,6 +1,36 @@
+import Ember from 'ember';
 import { mocha, describe, context, it, before, after, beforeEach, afterEach } from 'mocha';
 
 describe('mocha-shim', function() {
+
+  describe('beforeEach.run and afterEach.run', function() {
+    beforeEach.run(function() {
+      this.beforeEachRunInEmberRunLoop = Ember.run.currentRunLoop;
+    });
+
+    afterEach.run(function() {
+      window.chai.expect(Ember.run.currentRunLoop).to.be.ok;
+    });
+
+    it('runs the beforeEach.run hook inside the run loop', function() {
+      window.chai.expect(this.beforeEachRunInEmberRunLoop).to.be.ok;
+    });
+  });
+
+  describe('before.run and after.run', function() {
+    before.run(function() {
+      this.beforeEachRunInEmberRunLoop = Ember.run.currentRunLoop;
+    });
+
+    after.run(function() {
+      window.chai.expect(Ember.run.currentRunLoop).to.be.ok;
+    });
+
+    it('runs the before.run hook inside the run loop', function() {
+      window.chai.expect(this.beforeEachRunInEmberRunLoop).to.be.ok;
+    });
+  });
+
   it('should export global variables defined by mocha', function() {
     window.chai.expect(mocha).to.equal(window.mocha);
     window.chai.expect(describe).to.equal(window.describe);
