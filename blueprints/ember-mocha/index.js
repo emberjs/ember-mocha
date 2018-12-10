9ddf6f1e693e3e30e8f6592cc90269979bf44cc8
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-12-10 16:26:33 +0100

    blueprints: Automatically remove QUnit dependencies

--- blueprints/ember-mocha/index.js
+++ blueprints/ember-mocha/index.js
@@ -6,4 +6,14 @@ module.exports = {
     // not specified (since that doesn't actually matter
     // to us
   },
+
+  afterInstall() {
+    return Promise.resolve()
+      .then(() => this.removeQUnit());
+  },
+
+  removeQUnit() {
+    var packages = ['ember-cli-qunit', 'ember-qunit', 'qunit-dom'];
+    return this.removePackagesFromProject(packages.map(name => ({ name })));
+  },
 };
