e7582b5f2e1f6505a8728fee933d86b9a44a4ef2
Eli Flanagan <eflanagan@innovu.com>
2019-09-24 09:26:38 -0400

    better wording

--- tests/unit/setup-rendering-test-test.js
+++ tests/unit/setup-rendering-test-test.js
@@ -39,7 +39,7 @@ describe('setupRenderingTest', function() {
       expect(this.element.textContent.trim()).to.equal('Pretty Color: green');
     });
 
-    it('should pass when using this.set ', async function() {
+    it('fails when using this.set (but should pass)', async function() {
       this.set('name', 'red');
       await render(hbs`{{pretty-color name=name}}`);
       expect(this.element.textContent.trim()).to.equal('Pretty Color: red');
